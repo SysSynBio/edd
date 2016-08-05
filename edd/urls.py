5b72e2d490756c4edd0c45f685d195d7638547ea
William Morrell <wcmorrell@lbl.gov>
2016-08-04 18:12:12 -0700

    Haystack indexes must be defined in search_indexes

--- edd/urls.py
+++ edd/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url, static
 from django.contrib import admin
-from django.conf import settings
 
 admin.autodiscover()
 
@@ -16,9 +15,11 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),  # allauth does not support namespacing
     url(r'^utilities/', include('edd_utils.urls', namespace='edd_utils')),
     url(r'^profile/', include('edd.profile.urls', namespace='profile')),
+    url(r'^haystack/', include('haystack.urls', namespace='haystack')),
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.PUBLISH_REST_API:
     urlpatterns.append(url(r'^rest/', include('edd.rest.urls', namespace='rest')))
-    urlpatterns.append(url(r'^rest/auth/', include('rest_framework.urls',
-                               namespace='rest_framework')))
+    urlpatterns.append(
+        url(r'^rest/auth/', include('rest_framework.urls', namespace='rest_framework'))
+    )
---
+++ main/search_indexes.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+from django.db.models import F
+from haystack import indexes
+
+from . import models
+
+
+logger = logging.getLogger(__name__)
+
+
+class MetaboliteIndex(indexes.SearchIndex, indexes.Indexable):
+    # All SearchIndex objects must define one and only one Field with document=True
+    # default template located in {TEMPLATE_DIR}/search/indexes/{APP}/{MODEL}_{FIELD}.txt
+    # ./main/templates/search/indexes/main/Metabolite_text.txt
+    text = indexes.CharField(document=True, use_template=True)
+    # searchable fields
+    name = indexes.CharField(model_attr='type_name')
+    code = indexes.CharField(model_attr='short_name', null=True)
+    charge = indexes.IntegerField(model_attr='charge', null=True,)
+    carbons = indexes.IntegerField(model_attr='carbon_count', null=True)
+    mass = indexes.DecimalField(model_attr='molar_mass', null=True)
+    tags = indexes.MultiValueField(model_attr='tags', null=True, faceted=True)
+    source = indexes.CharField(model_attr='_source_name', faceted=True)
+    # autocomplete field
+    # ./main/templates/search/indexes/main/Metabolite_auto.txt
+    auto = indexes.NgramField(use_template=True)
+
+    def getModel(self):
+        return models.Metabolite
+
+    def index_queryset(self, using=None):
+        # the using parameter is defined but un-used by the API
+        return models.Metabolite.objects.annotate(_source_name=F('source__name'))
