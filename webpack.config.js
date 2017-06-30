f1dd7ecefadad77e8e33eebb04381c270b079b58
Traci Lopez <tlopez@lbl.gov>
2017-06-29 17:47:24 -0700

    compile js file for each typescript file

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,34 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: "./typescript/src/",
+    devtool: 'source-map',
+    entry: {
+        AssayTableDataGraphing: "./typescript/src/AssayTableDataGraphing.ts",
+        BiomassCalculationUI: "./typescript/src/BiomassCalculationUI.ts",
+        CarbonSummation: "./typescript/src/CarbonSummation.ts",
+        DataGrid: "./typescript/src/DataGrid.ts",
+        Dragboxes: "./typescript/src/Dragboxes.ts",
+        EDDAutocomplete: "./typescript/src/EDDAutocomplete.ts",
+        EDDDataInterface: "./typescript/src/EDDDataInterface.ts",
+        EDDEditableElement: "./typescript/src/EDDEditableElement.ts",
+        EDDGraphingTools: "./typescript/src/EDDGraphingTools.ts",
+        EDDRest: "./typescript/src/EDDRest.ts",
+        ExperimentDescHelp: "./typescript/src/Experiment-Desc-Help.ts",
+        FileDropZone: "./typescript/src/FileDropZone.ts",
+        Import: "./typescript/src/Import.ts",
+        index: "./typescript/src/index.ts",
+        StudyCreate: "./typescript/src/Study-Create.ts",
+        StudyData: "./typescript/src/Study-Data.ts",
+        StudyLines: "./typescript/src/Study-Lines.ts",
+        StudyOverview: "./typescript/src/Study-Overview.ts",
+        Study: "./typescript/src/Study.ts",
+        StudyCarbonBalance: "./typescript/src/StudyCarbonBalance.ts",
+        StudySBMLExport: "./typescript/src/StudySBMLExport.ts",
+        Utl: "./typescript/src/Utl.ts"
+    },
     output: {
-        path: './bin',
-        filename: "bundle.js"
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
     },
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".js", ".vue"],
