8519bb52031fcffce78fe4690e1b8189c3d2baf4
Traci Lopez <tlopez@lbl.gov>
2017-07-17 13:06:28 -0700

    remove components

--- typescript/src/import2.ts
+++ typescript/src/import2.ts
@@ -18,70 +18,54 @@ Vue.use(VueFormWizard);
 //     }
 // }
 
-var category = {
-    data: function () {
-    return {
-      categoryList: [
-          {name: 'Proteomics'},
-          { name: 'Metabolomics'},
-          { name: 'Transcriptomics'},
-          { name: 'OD600'},
-          { name: 'Other'}
-      ],
-      proteomics: [
-          { name: 'JBEI Targeted Proteomics'},
-          { name: 'PNNL Targeted Proteomics'},
-          { name: 'JBEI Shotgun Proteomics'},
-      ],
-      metabolomics : [
-          { name: 'JBEI Targeted Metabolomics'},
-          { name: 'PNNL Targeted Metabolomics'},
-      ],
-      selectedCategory: '',
-    }
-    },
-    template: '<div class="categories">' +
-                '<div><button v-for="item in categoryList" v-on:click.prevent="selectProtocol">' +
-                '{{ item.name }}</button></div>' +
-                '<div><h2>{{ selectedCategory }}</h2></div>' +
-             '</div>',
-    methods: {
-        selectProtocol: function(event) {
-            $('.categories').find('button').css('color', 'grey');
-            event.target.style.color = 'blue';
-            this.selected = $(event.target).text();
-            this.$emit('protocol', this.selected);
-            $('#protocols').show();
-        },
-    },
-};
-
-var protocol = {
-    data: function () {
-    return {
-      proteomics: [
-          { name: 'JBEI Targeted Proteomics'},
-          { name: 'PNNL Targeted Proteomics'},
-          { name: 'JBEI Shotgun Proteomics'},
-      ],
-      metabolomics : [
-          { name: 'JBEI Targeted Metabolomics'},
-          { name: 'PNNL Targeted Metabolomics'},
-      ],
-        selectedProtocol: category.data()
-    }
-    },
-    template: '<div id="protocols" hidden><h3>What Protocol did you use?</h3><div><button v-for="item in proteomics" v-on:click.prevent="selectProtocol">' +
-              '{{ item.name }}</button></div><div><h2>{{ selectedProtocol.selectedCategory }}</h2></div></div>',
-    methods: {
-        selectProtocol: function(event) {
-            $('#protocols').find('button').css('color', 'grey');
-            event.target.style.color = 'blue';
-            this.selected = $(event.target).text();
-            $('#fileFormat').show();
-        },
-    },
-};
+// var category = {
+//     data: function () {
+//     return {
+//       categoryList: [
+//           {name: 'Proteomics'},
+//           { name: 'Metabolomics'},
+//           { name: 'Transcriptomics'},
+//           { name: 'OD600'},
+//           { name: 'Other'}
+//       ],
+//       selectedCategory: '',
+//     }
+//     },
+//     template: '<div class="categories">' +
+//                 '<div><button v-for="item in categoryList" v-on:click.prevent="selectProtocol">' +
+//                 '{{ item.name }}</button></div>' +
+//                 '<div><h2>{{ selectedCategory }}</h2></div>' +
+//              '</div>',
+//     methods: {
+//
+//     },
+// };
+//
+// var protocol = {
+//     data: function () {
+//     return {
+//       proteomics: [
+//           { name: 'JBEI Targeted Proteomics'},
+//           { name: 'PNNL Targeted Proteomics'},
+//           { name: 'JBEI Shotgun Proteomics'},
+//       ],
+//       metabolomics : [
+//           { name: 'JBEI Targeted Metabolomics'},
+//           { name: 'PNNL Targeted Metabolomics'},
+//       ],
+//         selectedProtocol: category.data()
+//     }
+//     },
+//     template:
+//     methods: {
+//         selectProtocol: function(event) {
+//             $('#protocols').find('button').css('color', 'grey');
+//             event.target.style.color = 'blue';
+//             this.selected = $(event.target).text();
+//             $('#fileFormat').show();
+//         },
+//     },
+// };
 
 window.addEventListener('load', function () {
     var parent = new Vue({
@@ -90,17 +74,17 @@ window.addEventListener('load', function () {
         data: {
             loadingWizard: false,
             highlight: false,
-            protocol: 'test'
-        },
-        components: {
-            // <my-component> will only be available in parent's template
-            'category-selector': category,
-            'protocol-selector': protocol
-        },
-        selectedCategory: {
-            selectedButton: function() {
-                return this.selected;
-            }
+            selectedProtocol: '',
+            selectedCategory: '',
+            proteomics: [
+              { name: 'JBEI Targeted Proteomics'},
+              { name: 'PNNL Targeted Proteomics'},
+              { name: 'JBEI Shotgun Proteomics'},
+            ],
+            metabolomics : [
+              { name: 'JBEI Targeted Metabolomics'},
+              { name: 'PNNL Targeted Metabolomics'},
+            ],
         },
         methods: {
             onComplete: function () {
@@ -121,7 +105,19 @@ window.addEventListener('load', function () {
              },
             highlightOnClick:function(event, category) {
                 alert(event + " " + category);
-            }
+            },
+            selectCategory: function(event) {
+                $('.categories').find('button').css('color', 'grey');
+                event.target.style.color = 'blue';
+                this.selectedCategory = $(event.target).text();
+                $('#protocols').show();
+            },
+            selectProtocol: function(event) {
+                $('#protocols').find('button').css('color', 'grey');
+                event.target.style.color = 'blue';
+                this.selectedProtocol = $(event.target).text();
+                $('#fileFormat').show();
+            },
         },
 
     })
