36110cf53a8202c1d1b6fea10c829c43b7c77991
Traci Lopez <tlopez@lbl.gov>
2017-08-09 17:17:22 -0700

    Step 1 complete.

--- typescript/src/import2.ts
+++ typescript/src/import2.ts
@@ -19,6 +19,7 @@ var dropzone = {
   },
     methods: {
         prepareDropzone: function() {
+            $('.wizard-btn').prop('disabled', true);
             //doing this b/c i can't figure out why 2 text areas exist
             if ($('.fd-zone').length > 1) {
                 let elem = $('.fd-zone')[1];
@@ -41,7 +42,8 @@ var dropzone = {
             var rows = [];
             var rawText = $('#step2textarea2').val();
 
-            var longestRow = rawText.split(/[ \r]*\n/).reduce((prev: number, rawRow: string): number => {
+            var longestRow = rawText.split(/[ \r]*\n/)
+                .reduce((prev: number, rawRow: string):number => {
                 var row: string[];
                 if (rawRow !== '') {
                     row = rawRow.split('\t');
@@ -73,15 +75,17 @@ var dropzone = {
         },
 
         fileReturnedFromServer: function(fileContainer, result, response){
-            
+            //handle csv files
             if (response.file_type === 'csv') {
                 this.rawText(response.file_data);
                 this.csvOutput = this.handleCSV();
-                this.csvOutput.input = this.csvOutput.input.map(function(d) {return d[0].split(',')});
+                this.csvOutput.input = this.csvOutput.input.map(function(d) {
+                    return d[0].split(',')
+                });
                 this.showDetailModal();
                 return
             }
-            //this works! :)
+            //handle xlsx files
             if (response.file_type === "xlsx") {
                 var ws = response.file_data["worksheets"][0];
                 this.csvOutput = ws;
@@ -110,22 +114,38 @@ window.addEventListener('load', function () {
             highlight: false,
             selectedProtocol: '',
             selectedCategory: '',
+            selectedFormat: '',
             mode: '',
-            step1: [
-                    {'Proteomics': [
-                        {'JBEI Targeted Promeomics': ['Skyline'],},
-                        {'PNNL Global Proteomics': ['Skyline'],}]},
-                    {'Metabolomics': [
-                        {'JBEI Targeted Metabolomics': ['Skyline']},
-                        {'PNNL Global Metabolomics': ['Skyline']}]},
-                    {'Transcriptomics': [
-                        {'JBEI Transcriptomics': ['Cufflinks file', 'Generic Import File']}
-                        ]},
-                    {'Other': [
-                        {'BioLector': ['BioLector XML file']}]}
-            ],
+            importOptions: {
+                categories: [
+                    {id: 'abcd', name: 'Proteomics'},
+                    {id: 'dcba', name: 'Metabolomics'},
+                    {id: 'badc', name: 'Transcriptomics'},
+                    {id: 'cdab', name: 'Other'}
+                ],
+                protocols: [
+                    {id: 'a123', category: 'abcd', formats: ['f123'], name: 'JBEI Targeted' +
+                    ' Proteomics'},
+                    {id: 'a456', category: 'abcd', formats: ['f123'], name: 'PNNL Global ' +
+                    'Proteomics'},
+                    {id: 'b123', category: 'dcba', formats: ['f123'], name: 'JBEI Targeted ' +
+                    'Metabolomics'},
+                    {id: 'b456', category: 'dcba', formats: ['f123'], name: 'PNNL Global ' +
+                    'Metabolomics'},
+                    {id: 'c123', category: 'badc', formats: ['f456', 'f789'], name: 'JBEI ' +
+                    'Transcriptomics'},
+                    {id: 'd123', category: 'cdab', formats: ['f024'], name: 'Biolector'}
+                ],
+                formats: [
+                    {id: 'f123', name: 'Skyline'},
+                    {id: 'f456', name: 'Cufflinks file'},
+                    {id: 'f789', name: 'Generic Import File'},
+                    {id: 'f024', name: 'Biolector XML File'},
+                ]
+            },
             categories: '',
             protocols: '',
+            formats: '',
             metabolomics : [
               { name: 'JBEI Targeted Metabolomics'},
               { name: 'PNNL Targeted Metabolomics'},
@@ -134,10 +154,6 @@ window.addEventListener('load', function () {
             importedData: [],
         },
         methods: {
-            prepareIt: function() {
-                this.categories = this.step1.map(function(d) {return Object.keys(d)[0]});
-                console.log(this.categories)
-            },
             clickedShowDetailModal: function (value) {
               if (value.input) {
                   value = value.input;
@@ -170,8 +186,9 @@ window.addEventListener('load', function () {
                 alert(event + " " + category);
             },
             selectCategory: function(event) {
-                $('.categories').find('button').css('color', 'grey');
-                event.target.style.color = 'blue';
+                $('.btn').css('color', 'lightgrey').css('border-color', 'lightgrey');
+                event.target.style.color = '#23527c';
+                $(event.target).css('border-color', '#23527c');
                 //get selected text and remove whitespace
                 this.selectedCategory = $(event.target).text().replace(/\s/g, '');
                 if ($(event.target).text() === 'Other') {
@@ -180,19 +197,46 @@ window.addEventListener('load', function () {
                 } else {
                     this.mode = 'std'
                 }
-                $('#protocols').show();
-                var t = this.step1.filter(function(d) {return (Object).keys(d)[0] === $(event.target).text().replace(/\s/g, '');});
-                this.protocols = ((<any>Object).values(t[0])[0]).map(function(d){return Object.keys(d)[0]});
+                $('#protocols').removeClass('off');
+                $(event.target).parent().find('p').remove();
+                let id = this.importOptions.categories.filter(function(d) {
+                    return d.name === $(event.target).text().replace(/\s/g, '');
+                });
+                id = id[0].id;
+                this.protocols = this.importOptions.protocols.filter(function(d) {
+                    return d.category === id
+                })
             },
             selectProtocol: function(event) {
-                $('#protocols').find('button').css('color', 'grey');
-                event.target.style.color = 'blue';
-                this.selectedProtocol = $(event.target).text();
-                $('#fileFormat').show();
+                //turn all buttons grey
+                $('#protocols').find('button').css('color','lightgrey').css('border-color','lightgrey');
+                //turn selected button blue
+                event.target.style.color = '#23527c';
+                $(event.target).css('border-color', '#23527c');
+                //remove white space before and after text.
+                this.selectedProtocol = $(event.target).text().replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+                //get selected protocol obj
+                var obj = this.importOptions.protocols.filter(function(d) {
+                    return d.name === $(event.target).text().replace(/^\s\s*/, '').replace(/\s\s*$/, '')
+                });
+                let id = obj[0].formats;
+                $(event.target).parent().find('p').remove();
+                let options = this.importOptions;
+                this.formats = id.map(function(d) {for(var key in options.formats)
+                                {if (options.formats[key].id === d) {return options.formats[key]}}});
+                if (this.formats.length === 1) {
+                   $('.wizard-btn ').prop('disabled', false);
+                }
+                $('#fileFormat').removeClass('off');
             },
-        },
-        mounted() {
-            this.prepareIt()
+            selectFormats: function(event) {
+                this.selectedFormat = $(event.target).text();
+                if (event.target.checked) {
+                    $('.wizard-btn ').prop('disabled', false);
+                } else {
+                    $('.wizard-btn ').prop('disabled', true);
+                }
+            },
         }
     })
 });
