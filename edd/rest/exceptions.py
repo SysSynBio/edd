b653f2c01f6cbd3b782c2162a15c1428eea3f57e
Mark Forrer <mark.forrer@lbl.gov>
2017-09-11 17:30:28 -0700

    Removed another artifact of earlier development

--- edd/rest/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""
-Defines custom Exception classes used by EDD's REST framework.
-"""
-
-def NotImplementedException(APIException):
-    status_code= 503
-
-    def init(detail):
-        super(NotImplementedException.__class__, self).__init__(detail)
